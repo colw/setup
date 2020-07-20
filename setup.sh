9d31ccd6ce2e0a6791551ed6c49d7ee8cabcd54f
colw <colw@outlook.com>
2020-07-20 21:53:04 +0200

    Create zshrc file

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-echo Installing Node via NPM…
+echo "Create .zshrc file if not already present"
+touch "${HOME}/.zshrc"
+
+echo "Installing Node via NPM…"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-echo Installing Brew…
+echo "Installing Brew…"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-echo Install from Brew…
+echo "Install from Brew…"
 brew install git hub tig wget jq dry
 brew cask install firefox google-chrome visual-studio-code emacs macvim alfred slack spotify itsycal dash
 
