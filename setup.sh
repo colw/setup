889b3b47c5526fc9265cd1b3ee1d013adbc648ed
colw <colw@outlook.com>
2020-07-20 21:50:18 +0200

    Add nvm, brew, tools, and apps

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-brew cask install visual-studio-code
+echo Installing Node via NPM…
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
+echo Installing Brew…
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+echo Install from Brew…
+brew install git hub tig wget jq dry
+brew cask install firefox google-chrome visual-studio-code emacs macvim alfred slack spotify itsycal dash
+
+echo "mkdir -p ${HOME}/code"
+mkdir -p "${HOME}/code"
