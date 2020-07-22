91782d9e26f76ab2092deea18f93b2ad2a9f8ddf
col <colw@outlook.com>
2020-07-22 12:10:53 +0200

    Various apps and plugins

--- setup.sh
+++ setup.sh
@@ -13,8 +13,23 @@ echo "Installing Brew…"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 echo "Install from Brew…"
-brew install git hub tig wget jq dry
-brew cask install gitup firefox google-chrome visual-studio-code sublime-text emacs macvim alfred slack spotify itsycal dash
+brew install git hub tig wget jq k9s mkcert nss
+brew cask install docker tunnelblick google-cloud-sdk gitup firefox google-chrome visual-studio-code sublime-text emacs macvim alfred slack spotify itsycal bitbar
+
+brew tap homebrew/cask-versions
+brew cask install dash4
+
+brew tap moncho/dry
+brew install dry
+
+# Install Chrome plugins
+# JSON Formatter — https://chrome.google.com/webstore/detail/json-formatter/bcjindcccaagfpapjjmafapmmgkkhgoa?hl=en
+
+# Install VS Code Plugins
+# vscode-styled-components https://marketplace.visualstudio.com/items?itemName=jpoissonnier.vscode-styled-components
+# vscode-graphql https://marketplace.visualstudio.com/items?itemName=Prisma.vscode-graphql
+# prettier https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
+# mjml https://marketplace.visualstudio.com/items?itemName=attilabuti.vscode-mjml
 
 echo "mkdir -p ~/code"
 mkdir -p ~/code
